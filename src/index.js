d4fd4b2f4acd6c53c25dcc42dcd90d0b14c1fbf0
HelenIsted <139465011+HelenIsted@users.noreply.github.com>
2024-03-07 16:54:32 +0000

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -14,8 +14,14 @@ function generatePoem(event) {
     "You have a lot of knowledge of english literature, your speciality being poetry. You can easily formulate short english poems when given a topic. Your mission is to generate a four line poem in basic HTML and seperate each line with a <br/>, do not inlcude a title to the poem, sign the poem with <strong>SheCodes AI</strong>";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   //
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = `<div class="generating">⌛ Generating a poem about ${instructionsInput.value} for you... </div>`;
+  poemElement.classList.remove("hidden");
+
+  //
   axios.get(apiURL).then(displayPoem);
 }
+
 //
 let poemform = document.querySelector("#poem-generator-form");
 poemform.addEventListener("submit", generatePoem);
